c8aefea9f0a60bd9817bcd145de0b59f8638b495
itutusausb <67843337+itutusausb@users.noreply.github.com>
2021-04-02 20:45:36 +0200

    Refactor en TestUnitario

--- P5/src/test/java/com/proogramers/uoc/P5/TestUnitarioProyectos.java
+++ P5/src/test/java/com/proogramers/uoc/P5/TestUnitarioProyectos.java
@@ -10,7 +10,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestUnitarioProyectos {
-	
+
+	ArrayList<Proyectos> listaProyectos = new ArrayList<>();
+	LocalDate iDate1 = LocalDate.parse("2000-01-01");
+	LocalDate fDate1 = LocalDate.parse("2002-02-02");
+
+	Proyectos p1 = new Proyectos("España", "Catalunya", "Nada", "Nada", iDate1, fDate1, "Antonio", "Jesus", 5000.00,
+			"Nada de accions", 5, 12);
 
 	@Before
 	public void setUp() throws Exception {
@@ -20,42 +26,23 @@ public class TestUnitarioProyectos {
 	@Test
 	public void testInsert() {
 
-		ArrayList<Proyectos> listaProyectos = new ArrayList<>();
-		LocalDate iDate1 = LocalDate.parse("2000-01-01");
-		LocalDate fDate1 = LocalDate.parse("2002-02-02");
-		Proyectos p1 = new Proyectos("España", "Catalunya", "Nada", "Nada", iDate1, fDate1, "Antonio", "Jesus", 5000.00,
-				"Nada de accions", 5, 12);
-
 		listaProyectos.add(p1);
-
 		assertEquals(listaProyectos.size(), 1); // verificamos que hay 1 elemento en la lista
-		
 
 	}
 
 	@Test
 	public void testDelete() {
 
-		ArrayList<Proyectos> listaProyectos = new ArrayList<>();
-		LocalDate iDate1 = LocalDate.parse("2000-01-01");
-		LocalDate fDate1 = LocalDate.parse("2002-02-02");
-		Proyectos p1 = new Proyectos("España", "Catalunya", "Nada", "Nada", iDate1, fDate1, "Antonio", "Jesus", 5000.00,
-				"Nada de accions", 5, 12);
-
 		listaProyectos.add(p1);
 		listaProyectos.remove(0);
 		assertEquals(listaProyectos.size(), 0); // verificamos que se ha eliminado de la lista
 
 	}
-	
+
 	@Test
 	public void testUpdate() {
 
-		ArrayList<Proyectos> listaProyectos = new ArrayList<>();
-		LocalDate iDate1 = LocalDate.parse("2000-01-01");
-		LocalDate fDate1 = LocalDate.parse("2002-02-02");
-		Proyectos p1 = new Proyectos("España", "Catalunya", "Nada", "Nada", iDate1, fDate1, "Antonio", "Jesus", 5000.00,
-				"Nada de accions", 5, 12);
 		System.out.println("------------P1--------------");
 		System.out.println(p1.toString());
 		System.out.println("----------------------------");
@@ -64,7 +51,7 @@ public class TestUnitarioProyectos {
 		System.out.println("---------P1 UPDATE----------");
 		System.out.println(listaProyectos.toString());
 		System.out.println("----------------------------");
-		assertEquals(p1, listaProyectos.get(0) ); // verificamos que se ha actualizado de la lista
+		assertEquals(p1, listaProyectos.get(0)); // verificamos que se ha actualizado de la lista
 
 	}
 }
