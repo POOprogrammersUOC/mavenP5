53fa8b5e228bbad283ef8b6a45a5f410eed39afe
lopoBCN <66134377+lopoBCN@users.noreply.github.com>
2021-04-03 23:08:09 +0200

    test unitario socios

---
+++ P5/src/test/java/com/proogramers/uoc/P5/TestUnitarioSocios.java
@@ -0,0 +1,55 @@
+package com.proogramers.uoc.P5;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUnitarioSocios {
+
+	ArrayList<Socios> listaSocios = new ArrayList<>();
+	
+	Socios s1 = new Socios("Juan", "Valdes", 123, "Barcelona", "965698986");
+			
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testInsert() {
+
+		listaSocios.add(s1);
+		assertEquals(listaSocios.size(), 1); 
+
+	}
+
+	@Test
+	public void testDelete() {
+
+		listaSocios.add(s1);
+		listaSocios.remove(0);
+		assertEquals(listaSocios.size(), 0); 
+
+	}
+
+	@Test
+	public void testUpdate() {
+
+		System.out.println("------------S1--------------");
+		System.out.println(s1.toString());
+		System.out.println("----------------------------");
+		listaSocios.add(s1);
+		s1.setNombre("Luis");
+		System.out.println("---------S1 UPDATE----------");
+		System.out.println(listaSocios.toString());
+		System.out.println("----------------------------");
+		assertEquals(s1, listaSocios.get(0)); 
+
+	}
+}
