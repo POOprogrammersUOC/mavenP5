6ac988a163b732dc484f8abde94df493814e35ae
lopoBCN <66134377+lopoBCN@users.noreply.github.com>
2021-04-03 19:53:10 +0200

    commit test socios listado

---
+++ P5/src/test/java/com/proogramers/uoc/P5/SociosTestListado.java
@@ -0,0 +1,32 @@
+package com.proogramers.uoc.P5;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Test;
+
+
+public class SociosTestListado {
+
+	
+	public void AddSociosTest() throws JAXBException {
+		
+		Socios soc1 = new Socios("Jose", "Martinez", 121, "Barcelona", "654859698");
+		Socios soc2 = new Socios("Anotnio", "Gonzalez", 122, "Granollers", "698745256");
+		
+		
+		List<Socios> listadoSocios = new ArrayList<Socios>();
+		assertEquals(0, listadoSocios.size());
+		listadoSocios.add(soc1); 
+		assertEquals(1, listadoSocios.size()); 
+		listadoSocios.add(soc2);
+		
+	}
+	
+	
+
+}
