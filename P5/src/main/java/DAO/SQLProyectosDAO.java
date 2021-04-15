2f43cc6448d333f4b5cec0c351e7657139041ff5
itutusausb <67843337+itutusausb@users.noreply.github.com>
2021-04-15 22:10:58 +0200

    función insertar acabada, no se ha probado

--- P5/src/main/java/DAO/SQLProyectosDAO.java
+++ P5/src/main/java/DAO/SQLProyectosDAO.java
@@ -13,12 +13,17 @@ import datosMysql.Conexion;
 public class SQLProyectosDAO {
 
 	private static final String SQL_SELECT = "SELECT Id_proyecto, Pais, Localizacion, LineaDeAccion, FechaInicio, FechaFinal, SocioLocal, Financiador, Financiacion, Acciones, Personal, VoluntariosAsignados, Ong_CIF FROM mydb.proyectos";
+	private static final String SQL_INSERT = "INSERT INTO mydb.proyectos(Pais, Localizacion, LineaDeAccion, FechaInicio, FechaFinal, SocioLocal, Financiador, Financiacion, Acciones, Personal, VoluntariosAsignados, Ong_CIF) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	
+	
+
 	
 	//******************************************************************************
 	// Método seleccionar, recoge toda la información de la tabla proyectos mysql
 	//******************************************************************************
 	
 	public List<Proyectos> seleccionar(){
+		
 		Connection conn = null;
 		PreparedStatement pstat = null;
 		ResultSet rs = null;
@@ -47,7 +52,7 @@ public class SQLProyectosDAO {
 				String acciones			= rs.getString("Acciones");
 				int personal			= rs.getInt("Personal");
 				int voluntariosAsignados= rs.getInt("VoluntariosAsignados");
-				String ongCif			= rs.getString("Ong_CIF");
+				//String ongCif			= rs.getString("Ong_CIF");
 				
 				proyectos = new Proyectos(pais, localizacion, lineaDeAccion, lineaDeAccion, fechaInicio, fechaFinal, socioLocal, financiador, financiacion, numProyecto, acciones, personal, voluntariosAsignados);
 				
@@ -72,5 +77,49 @@ public class SQLProyectosDAO {
 			
 		}
 		return listaProyectos;
+	}
+	
+	//******************************************************************************
+	// Método Insertar, inserta la información indicada a la base de datos
+	//******************************************************************************
+	
+	public int insertar(Proyectos proyectos) {
+		Connection conn = null;
+		PreparedStatement pstat = null;
+		int registros = 0;
+		try {
+			conn = getConection();
+			pstat = conn.prepareStatement(SQL_INSERT);
+			pstat.setString(1, proyectos.getPais());
+			pstat.setString(2, proyectos.getLocalizacion());
+			pstat.setString(3, proyectos.getLineaDeAccion());
+			pstat.setString(4, proyectos.getSublineaDeAccion());
+			//LocalDate fechaInicio = null;
+			//if (proyectos.getFechaInicio() != null) {						//*********REVISAR*********\\
+			pstat.setObject(5,proyectos.getFechaInicio());
+			pstat.setObject(6,proyectos.getFechaFinal());
+			pstat.setString(6, proyectos.getSocioLocal());
+			pstat.setString(7, proyectos.getFinanciador());
+			pstat.setDouble(8, proyectos.getFinanciacion());
+			pstat.setString(9, proyectos.getAcciones());
+			pstat.setInt(10, proyectos.getPersonal());
+			pstat.setInt(11, proyectos.getVoluntariosAsignados());
+			//pstat.setString(12, proyectos.getOngCif)); falta incluirla en la clase proyectos
+			//}
+			registros = pstat.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Bloque catch generado automáticamente
+			e.printStackTrace();
+		}finally {
+			try {
+				close(pstat);
+				close(conn);
+			} catch (SQLException e) {
+				// TODO Bloque catch generado automáticamente
+				e.printStackTrace();
+			}
+		}
+		return registros;
 	}
 }
