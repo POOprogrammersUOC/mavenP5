0a833ccbdbb98de03056ef6c15fef497178bbbbc
javentinuoc <byow@uoc.edu>
2021-04-27 23:52:51 +0200

    interface dao sql

---
+++ P5/src/main/java/DAOMySQL/ISociosDAOSQL.java
@@ -0,0 +1,16 @@
+package DAOMySQL;
+
+
+	import java.sql.SQLException;
+	import java.util.List;
+
+	import com.proogramers.uoc.P5.Socios;
+
+	public interface ISociosDAOSQL {
+
+		
+		public int insertar(Socios socios) throws SQLException;
+		public int actualizar(Socios socios) throws SQLException;
+		public int eliminar(Socios socios) throws SQLException;
+		public List<Socios> seleccionar() throws SQLException;
+}
