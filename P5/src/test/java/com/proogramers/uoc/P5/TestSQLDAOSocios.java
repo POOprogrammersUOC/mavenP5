58339ef40055472d7ea31643dc4ae669cceb3ff0
javentinuoc <byow@uoc.edu>
2021-04-28 01:59:34 +0200

    creacion clase test Socios SQL DAO

---
+++ P5/src/test/java/com/proogramers/uoc/P5/TestSQLDAOSocios.java
@@ -0,0 +1,18 @@
+package com.proogramers.uoc.P5;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import DAO.DAOFactory;
+import DAO.;
+import datosMysql.*;
+
+public class TestSQLDAOSocios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
