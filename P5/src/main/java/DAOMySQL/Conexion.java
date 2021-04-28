9371e2cbbe75cc1d95278997ed4f4649b33939cb
javentinuoc <byow@uoc.edu>
2021-04-28 20:39:38 +0200

    modificaciones clase conexion (adaptado a Isaac)

--- P5/src/main/java/DAOMySQL/Conexion.java
+++ P5/src/main/java/DAOMySQL/Conexion.java
@@ -2,38 +2,46 @@ package DAOMySQL;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Conexion {
 
-	protected Connection conexion;
-	
 
-	private final String DB_URL = "jdbc:mysql://localhost:3306/mydb";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/mydb";
 	
 	// DB credenciales
 	
-	private final String USER = "root";
-	private final String PASS = "admin";
+	private static final String USER = "root";
+	private static final String PASS = "admin";
 	
-	public void conectar() throws SQLException {
-		try {
-			conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+	public static Connection getConnection() throws SQLException {
+		
+			return DriverManager.getConnection(DB_URL, USER, PASS);
 			
-			
-		} catch(SQLException e) {
-			throw e;
-		}
+		
+		
 	}
 	
-	public void cerrar() throws SQLException {
-		if(conexion != null) {
-			if(conexion.isClosed()) {
-				conexion.close();
-			}
-		}
+	public static void close(ResultSet rs) throws SQLException {
+		
+	rs.close();
+		
 	}
-	
+
+	public static void close(PreparedStatement st) throws SQLException {
+		
+		st.close();
+	}
+
+	public static void close(Connection conn) throws SQLException {
+		
+		conn.close();
+	}
+
+
+
 }
 	
 	
